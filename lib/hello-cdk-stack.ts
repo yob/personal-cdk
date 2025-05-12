9223bbb938c7cde3d3aaaf8b02d56d51b71af685
James Healy <james.healy@amber.com.au>
2025-05-12 22:38:01 +1000

    swap example to S3, it's easier to work with

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,35 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Define the Lambda function resource
-    const myFunction = new lambda.Function(this, "HelloWorldFunction", {
-      runtime: lambda.Runtime.NODEJS_20_X, // Provide any supported Node.js runtime
-      handler: "index.handler",
-      code: lambda.Code.fromInline(`
-        exports.handler = async function(event) {
-          return {
-            statusCode: 200,
-            body: JSON.stringify('Hello CDK!'),
-          };
-        };
-      `),
-    });
-
-    // Define the Lambda function URL resource
-    const myFunctionUrl = myFunction.addFunctionUrl({
-      authType: lambda.FunctionUrlAuthType.NONE,
-    });
-
-    // Define a CloudFormation output for your URL
-    new cdk.CfnOutput(this, "myFunctionUrlOutput", {
-      value: myFunctionUrl.url,
-    })
+      const bucket = new s3.Bucket(this, `test-bucket-949194823831`, {
+        bucketName: `test-bucket-949194823831`,
+        objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+        blockPublicAccess: {
+          blockPublicPolicy: true,
+          blockPublicAcls: true,
+          ignorePublicAcls: true,
+          restrictPublicBuckets: true,
+        },
+      })
 
     cdk.Tags.of(this).add('repository', 'yob/foo');
   }
